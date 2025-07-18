1b47fd8936ae8055d28b55faca29127fa2f2213b
unknown <hamzakhalem97@gmail.com>
2025-07-18 16:26:50 +0100

    feat: update splash screeb

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer, DarkTheme as NavigationDarkTheme } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
-import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator, Image } from 'react-native';
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -16,6 +16,11 @@ export default function App() {
   if (loading) {
     return (
       <View style={styles.splashContainer}>
+        <Image 
+          source={require('./src/assets/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
         <Text style={styles.splashText}>CookinApp</Text>
         <ActivityIndicator size="large" color="#ff9800" />
       </View>
@@ -35,6 +40,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#181A20',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
   },
   splashText: {
     fontSize: 24,
